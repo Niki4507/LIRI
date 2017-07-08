a9a2e53c5a59973723092b452fb602e40ba782ce
Niki Tidmore <nikitidmore@nikis-mbp.attlocal.net>
2017-07-08 18:31:47 -0500

    first commit

---
+++ keys.js
@@ -0,0 +1,22 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<input here>',
+  consumer_secret: '<input here>',
+  access_token_key: '<input here>',
+  access_token_secret: '<input here>',
+}
+
+exports.spotifyKeys = {
+  consumer_key: '<input here>',
+  consumer_secret: '<input here>',
+  access_token_key: '<input here>',
+  access_token_secret: '<input here>',
+}
+
+exports.omdbKeys = {
+  consumer_key: '<input here>',
+  consumer_secret: '<input here>',
+  access_token_key: '<input here>',
+  access_token_secret: '<input here>',
+}
